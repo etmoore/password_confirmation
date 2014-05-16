320daddba3659006708051acfc788b9dbd24f791
Evan Moore <etmoore@gmail.com>
2014-05-16 14:51:43 -0600

    created passwordEvent function to handle character length

--- js/app.js
+++ js/app.js
@@ -2,12 +2,21 @@
 //Solution: Hide and show them at appropriate times
 
 //Hide Hints
+$("form span").hide();
+
+function passwordEvent(){
+  //Find out if password is valid
+  if($(this).val().length > 8) {
+    //Hind hint if valid
+    $(this).next().hide();
+  } else {
+    //else show hint
+    $(this).next().show();
+  }
+}
 
 //When event happens on password input
-  //Find out if password is valid
-    //Hind hint if valid
-    //else show hint
-
+$("#password").focus(passwordEvent).keyup(passwordEvent);
 
 //When event happens on confimration input
   //Find out if password and confirmation match
